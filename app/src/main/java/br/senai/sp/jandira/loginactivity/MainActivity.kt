9cbbc00129d0b8b6e95a875fe3b7baa9374b9ec7
ARTUR DE MIRANDA ALVES <22282175@dsn127m1246729.cfp127aluno.local>
2023-03-07 16:43:44 -0300

    Login semi-concluido

---
+++ app/src/main/java/br/senai/sp/jandira/loginactivity/MainActivity.kt
@@ -0,0 +1,157 @@
+package br.senai.sp.jandira.loginactivity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.loginactivity.ui.theme.LoginActivityTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginActivityTheme {
+                LoginScreen()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreen() {
+    var emailState = rememberSaveable {
+        mutableStateOf("")
+    }
+    var passwordState = rememberSaveable {
+        mutableStateOf("")
+    }
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        // Header
+        Column(modifier = Modifier
+            .fillMaxWidth()
+        ) {
+            Row(modifier = Modifier
+                .fillMaxWidth(),
+                horizontalArrangement =  Arrangement.End) {
+                Surface(modifier = Modifier
+                    .width(120.dp)
+                    .height(40.dp),
+                    color = Color(207, 6, 240, 240),
+                    shape = RoundedCornerShape(bottomStart = 20.dp)
+                ) {
+
+                }
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 220.dp, start = 30.dp)
+
+        ) {
+            Text(
+                text = stringResource(id = R.string.title),
+                fontSize = 48.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(207, 6, 240, 240)
+            )
+            Text(
+                text = stringResource(id = R.string.subtitle),
+                color = Color(160, 156, 156)
+            )
+        }
+
+        // Form
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 380.dp, start = 30.dp)
+        ) {
+            OutlinedTextField(
+                value = stringResource(id = R.string.email_example),
+                onValueChange = {
+                    emailState
+                }, shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 17.dp)
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            OutlinedTextField(
+                value = stringResource(id = R.string.password_example),
+                onValueChange = {
+                    passwordState
+                }, shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 17.dp),
+
+            )
+        }
+
+        // Footer
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 600.dp, start = 150.dp)
+        )
+        {
+            Button(onClick = {},
+                modifier = Modifier
+                    .padding(start = 100.dp)
+                    .width(120.dp)
+                    .height(48.dp),
+                    shape = RoundedCornerShape(15.dp),
+                    colors = ButtonDefaults.buttonColors(Color(207, 6, 240, 240)),
+
+            ) {
+                Text(text = stringResource(id = R.string.sign_in), color = Color.White)
+            }
+
+            Row() {
+                Text(modifier = Modifier
+                    .padding(start = 50.dp, top = 30.dp),
+                    color = Color(160, 156, 156)
+                    ,text = stringResource(id = R.string.account))
+//               Button(onClick = { /*TODO*/ }) {
+//                }
+            }
+        }
+
+        Column(modifier = Modifier
+            .fillMaxWidth().padding(top = 811.dp)
+        ) {
+            Row(modifier = Modifier
+                .fillMaxWidth(),
+                horizontalArrangement =  Arrangement.Start) {
+                Surface(modifier = Modifier
+                    .width(120.dp)
+                    .height(40.dp),
+                    color = Color(207, 6, 240, 240),
+                    shape = RoundedCornerShape(topEnd = 20.dp)
+                ) {
+
+                }
+            }
+        }
+
+
+    }
+}
